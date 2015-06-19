1ab60819d345995456d4db5fb45908bd9576e7b1
Greg Flynn <flynng@lafayette.edu>
2015-06-19 11:58:33 -0400

    Fixed FSM logic
    Was using bad logic that required 2 clock cycles to work. Fixed now only
    require 1 cycle to updated cell

--- srcs/life_cell.v
+++ srcs/life_cell.v
@@ -5,16 +5,17 @@
 // File          : life_cell.v
 // Author        : John Nestor
 // Created       : 05.03.2013
-// Last modified : 05.03.2013
+// Last modified : 06.19.2015
+// Modified by   : Greg Flynn
 //-----------------------------------------------------------------------------
 // Description :
 // Basic cell for calculating next state in Conway's Life
 //------------------------------------------------------------------------------
 // Modification history :
 // 05.03.2013 : created
+// 06.19.2015 : added in scan feature
 //-----------------------------------------------------------------------------
 
-////  TODO: add an "operation" input that allows hold, read, write???
 
 module life_cell(input clk,
                         reset,
@@ -29,23 +30,42 @@ module life_cell(input clk,
                         write, // enable writing
                         val, // to set the cell to a certain value
                         enb, // enable the cell to come to life
+								scan, // enable scan
+								scan_val, // the value to write when scanning
+											 //(normally east cell)
 		                output reg alive
 		                );
 
     wire [3:0] 		neighbor_count;
-    reg alive_next;
+    reg 				alive_next;
 
     assign 		neighbor_count = n + ne + e + se + s + sw + w + nw;
 
-    always @(posedge clk)  begin alive <= alive_next;
-      if (reset) alive_next <= 0;
-      else if (write) alive_next <= val;
-		else if(!enb); // do nothing
-      else if (alive)
-        if (neighbor_count < 2 || neighbor_count >3 ) alive_next <= 0;
-        else alive_next <= 1;
-      else if (neighbor_count == 3) alive_next <= 1;
-   end
+    always @(posedge clk) begin 
+	   if (scan) begin
+				alive <= scan_val;
+				alive_next = scan_val;
+				end
+		else if (write) begin
+				alive <= val;
+				alive_next = val;
+				end
+		else if (reset) begin
+				alive <= 0;
+				alive_next = 0;
+				end
+			
+		else alive <= alive_next;
+	end
+	
+	always @* begin
+		if (enb) begin
+				if (alive)
+					if (neighbor_count < 2 || neighbor_count > 3 ) alive_next = 0;
+					else alive_next = 1;
+					else if (neighbor_count == 3) alive_next = 1;
+		end
+	end
 
 endmodule // life_cell
 

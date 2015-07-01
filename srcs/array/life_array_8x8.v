bac1a2af3b9b29489c4a002b9e7e53840ce6f771
Greg Flynn <flynng@lafayette.edu>
2015-07-01 15:09:46 -0400

    Connected 4, 4x4 together.  Testing now, Working on #23

--- srcs/array/life_array_8x8.v
+++ srcs/array/life_array_8x8.v
@@ -53,13 +53,34 @@ module life_array_8x8(
 					tile_1_W = 4'b0;
 					
     wire [3:0] tile_2_N = 4'b0,
-	            tile_2_E =, tile_2_S, tile_2_W;
-    wire [3:0] tile_3_N, tile_3_E, tile_3_S, tile_3_W;
+	            tile_2_E = 4'b0,
+					tile_2_S = {alive_3[0],alive_3[4],alive_3[8],alive_3[12]},
+					tile_2_W = alive_0[12:15];
+					
+    wire [3:0] tile_3_N = {alive_2[3],alive_2[7],alive_2[11],alive_2[15]},
+               tile_3_E = 4'b0,
+					tile_3_S = 4'b0,
+					tile_3_W = alive_1[12:15];
 	 
-	 wire tile_0_NW, tile_0_NE, tile_0_SE, tile_0_SW;
-	 wire tile_1_NW, tile_1_NE, tile_1_SE, tile_1_SW;
-	 wire tile_2_NW, tile_2_NE, tile_2_SE, tile_2_SW;
-	 wire tile_3_NW, tile_3_NE, tile_3_SE, tile_3_SW;
+	 wire tile_0_NW = 0,
+	      tile_0_NE = 0,
+			tile_0_SE = alive_3[0],
+			tile_0_SW = 0;
+			
+	 wire tile_1_NW = 0,
+	      tile_1_NE = alive_2[3],
+			tile_1_SE = 0,
+			tile_1_SW = 0;
+			
+	 wire tile_2_NW = 0,
+	      tile_2_NE = 0,
+			tile_2_SE = 0,
+			tile_2_SW = alive_1[12];
+			
+	 wire tile_3_NW = alive_0[15],
+	      tile_3_NE = 0,
+			tile_3_SE = 0,
+			tile_3_SW = 0;
     
 	 assign write_0 = write_enb & (vali_selector == 2'b00);
 	 assign write_1 = write_enb & (vali_selector == 2'b01);
@@ -73,29 +94,63 @@ module life_array_8x8(
 										.val(vali),
 										.write_enb(write_0),
 										.step(step),
-										.n(4'b0),
-										.e(edge_0_2),
-										.s(edge_0_1),
-										.w(4'b0),
-										.nw(1'b0),
-										.ne(1'b0),
-										.se(edge_0_3),
-										.sw(1'b0)
-										
-                              );
+										.n(tile_0_N),
+										.e(tile_0_E),
+										.s(tile_0_S),
+										.w(tile_0_W),
+										.nw(tile_0_NW),
+										.ne(tile_0_NE),
+										.se(tile_0_SE),
+										.sw(tile_0_SW)
+										);
     
-    life_array_4x4 U_array_1 (.clk(clk),
-	                           .reset(reset),
+	 life_array_4x4 U_array_1 (.clk(clk),
+                              .reset(reset),
 										.alive(alive_1),
 										.val(vali),
 										.write_enb(write_1),
 										.step(step),
-										.n()
+										.n(tile_1_N),
+										.e(tile_1_E),
+										.s(tile_1_S),
+										.w(tile_1_W),
+										.nw(tile_1_NW),
+										.ne(tile_1_NE),
+										.se(tile_1_SE),
+										.sw(tile_1_SW)
 										);
     
-    life_array_4x4 U_array_2 ();
-    
-    life_array_4x4 U_array_3 ();
+	 life_array_4x4 U_array_2 (.clk(clk),
+                              .reset(reset),
+										.alive(alive_2),
+										.val(vali),
+										.write_enb(write_2),
+										.step(step),
+										.n(tile_2_N),
+										.e(tile_2_E),
+										.s(tile_2_S),
+										.w(tile_2_W),
+										.nw(tile_2_NW),
+										.ne(tile_2_NE),
+										.se(tile_2_SE),
+										.sw(tile_2_SW)
+										);
+										
+    life_array_4x4 U_array_3 (.clk(clk),
+                              .reset(reset),
+										.alive(alive_3),
+										.val(vali),
+										.write_enb(write_3),
+										.step(step),
+										.n(tile_3_N),
+										.e(tile_3_E),
+										.s(tile_3_S),
+										.w(tile_3_W),
+										.nw(tile_3_NW),
+										.ne(tile_3_NE),
+										.se(tile_3_SE),
+										.sw(tile_3_SW)
+										);
 
 
 endmodule

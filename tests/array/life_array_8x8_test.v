b2da12fdd9a8cf76aa380bde0f50c5a96a69b759
Greg Flynn <flynng@lafayette.edu>
2015-07-01 16:06:43 -0400

    Error with testing

--- tests/array/life_array_8x8_test.v
+++ tests/array/life_array_8x8_test.v
@@ -45,7 +45,8 @@ module life_array_8x8_test;
 	wire [15:0] valo;
     
     // Expected value
-
+    reg [15:0] expected_alive = 16'h0000;
+    
 	// Instantiate the Unit Under Test (UUT)
 	life_array_8x8 uut (
 		.clk(clk), 
@@ -92,7 +93,122 @@ module life_array_8x8_test;
         reset = 0;
         #10;
         
+        // attempt to configure array 0 (TL) with an unstable config
+        
+        vali = 16'h0001; // select top left cell in the array
+        vali_selector = 2'b00; // top left
+        #10;
+        write_enb = 1;
+        valo_selector = 0;
+        #10;
+        write_enb = 0;
+        expected_alive = 16'h0001;
+        if(expected_alive != valo) begin
+		    $display("Failed to write to top left cell only");
+		    $stop;
+		end
+        $display("Basic configuration passed");
+        
+        // try to kill the cell
+        #10;
+        step = 1;
+        #10;
+        expected_alive = 16'h0000;
+        if(expected_alive != valo) begin
+		    $display("Single cell failed to die");
+		    $stop;
+		end
+        $display("Cell death passes");
+        step = 0;
+        #10;
+        
+        // Create a block with one cell in each 4x4
+        
+        vali = 16'h8000; // bottom right cell
+        vali_selector = 2'b00; // top left 4x4
+        write_enb = 1;
+        #10;
+        vali = 16'h0008; // bottom left cell
+        vali_selector = 2'b10; // top right 4x4
+        #10;
+        vali = 16'h0001; // top left cell
+        vali_selector = 2'b11; // bottom right 4x4
+        #10;
+        vali = 16'h1000; // top right cell
+        vali_selector = 2'b01; // bottom left 4x4
+        #10;
+        write_enb = 0;
+        
+        // Error testing in this set up
+        valo_selector = 2'b00;
+        expected_alive = 16'h8000;
+        if(expected_alive != valo) begin
+		    $display("Top left array for block failed to configure correctly");
+		    $stop;
+		end
+        valo_selector = 2'b01;
+        #10;
+        expected_alive = 16'h1000;
+        if(expected_alive != valo) begin
+		    $display("Bottom left array for block failed to configure correctly");
+		    $stop;
+		end
+        valo_selector = 2'b10;
+        #10;
+        expected_alive = 16'h0008;
+        if(expected_alive != valo) begin
+		    $display("Top right array for block failed to configure correctly");
+		    $stop;
+		end
+        valo_selector = 2'b11;
+        #10;
+        expected_alive = 16'h0001;
+        if(expected_alive != valo) begin
+		    $display("Bottom right array for block failed to configure correctly");
+		    $stop;
+		end
+        
+        $display("Block configuration passed");
+        
+        // see if it is stable
+        step = 1;
+        #10;
+        step = 0;
+        
+        // Error testing after step
+        valo_selector = 2'b00;
+        #10;
+        expected_alive = 16'h8000;
+        if(expected_alive != valo) begin
+		    $display("Top left array for block failed to stabilize");
+		    $stop;
+		end
+        valo_selector = 2'b01;
+        #10;
+        expected_alive = 16'h1000;
+        if(expected_alive != valo) begin
+		    $display("Bottom left array for block failed to stabilize");
+		    $stop;
+		end
+        valo_selector = 2'b10;
+        #10;
+        expected_alive = 16'h0008;
+        if(expected_alive != valo) begin
+		    $display("Top right array for block failed to stabilize");
+		    $stop;
+		end
+        valo_selector = 2'b11;
+        #10;
+        expected_alive = 16'h0001;
+        if(expected_alive != valo) begin
+		    $display("Bottom right array for block failed to stabilize");
+		    $stop;
+		end
+        
+        
         $stop;
+        
+        
 	end
       
 endmodule

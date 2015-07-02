db382b0ca45f2c03cc99f94ec61f9ce92100b045
Greg Flynn <flynng@lafayette.edu>
2015-07-02 14:28:46 -0400

    Implemented previous alive to entire 4x4 array, working on #30

--- srcs/array/life_array_4x4.v
+++ srcs/array/life_array_4x4.v
@@ -18,6 +18,7 @@
 module life_array_4x4(input clk,
 		      input reset,
 		      output [15:0] alive,
+              output [15:0] alive_prev,
 		      input [15:0] val,
 		      input write_enb,
 		      input step, // set this high to start running the tiles
@@ -34,42 +35,44 @@ module life_array_4x4(input clk,
 
 
     // toggle
-	 wire run;
-	 reg toggle;
+    wire run;
+    reg toggle;
 	 
-	 assign run = step & ~toggle;
+	assign run = step & ~toggle;
 	 
-	 always @(posedge clk) begin
-	     if(reset) toggle <= 0;
-		  else if(write_enb) toggle <= 0;
-		  else toggle <= step;
+	always @(posedge clk) begin
+	    if(reset) toggle <= 0;
+		else if(write_enb) toggle <= 0;
+		else toggle <= step;
     end
 	 
     // internal connections
     wire [3:0] alive_col0, alive_col1, alive_col2, alive_col3;
+    wire [3:0] alive_prev_col0, alive_prev_col1, alive_prev_col2, alive_prev_col3;
 	 
-	 assign alive = {alive_col3,alive_col2,alive_col1,alive_col0};
+	assign alive = {alive_col3,alive_col2,alive_col1,alive_col0};
+	assign alive_prev = {alive_prev_col3,alive_prev_col2,alive_prev_col1,alive_prev_col0};
 	 
 
     life_col4 COL0 (.clk(clk), .reset(reset), .n(n[0]), .ne(n[1]), .e_col(alive_col1),
 		 .se(s[1]), .s(s[0]) , .sw(sw), .w_col(w), .nw(nw),
 		 .val(val[3:0]),  .write_enb(write_enb), .enable(run),
- 		 .alive_col(alive_col0));
+ 		 .alive_col(alive_col0),.alive_prev_col(alive_prev_col0));
 
     life_col4 COL1 (.clk(clk), .reset(reset), .n(n[1]), .ne(n[2]), .e_col(alive_col2),
 		 .se(s[2]), .s(s[1]) , .sw(s[0]), .w_col(alive_col0), .nw(n[0]),
 		 .val(val[7:4]), .write_enb(write_enb), .enable(run),
- 		 .alive_col(alive_col1));
+ 		 .alive_col(alive_col1),.alive_prev_col(alive_prev_col1));
 
     life_col4 COL2 (.clk(clk), .reset(reset), .n(n[2]), .ne(n[3]), .e_col(alive_col3),
-       .se(s[3]), .s(s[2]) , .sw(s[1]), .w_col(alive_col1), .nw(n[1]),
-       .val(val[11:8]), .write_enb(write_enb), .enable(run),
- 		 .alive_col(alive_col2));
+         .se(s[3]), .s(s[2]) , .sw(s[1]), .w_col(alive_col1), .nw(n[1]),
+         .val(val[11:8]), .write_enb(write_enb), .enable(run),
+ 		 .alive_col(alive_col2),.alive_prev_col(alive_prev_col2));
  		 
     life_col4 COL3 (.clk(clk), .reset(reset), .n(n[3]), .ne(ne), .e_col(e),
-       .se(se), .s(s[3]) , .sw(s[2]), .w_col(alive_col2), .nw(n[2]),
-       .val(val[15:12]), .write_enb(write_enb), .enable(run),
-       .alive_col(alive_col3));
+         .se(se), .s(s[3]) , .sw(s[2]), .w_col(alive_col2), .nw(n[2]),
+         .val(val[15:12]), .write_enb(write_enb), .enable(run),
+         .alive_col(alive_col3),.alive_prev_col(alive_prev_col3));
                  
 
 

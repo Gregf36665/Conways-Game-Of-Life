965823536f5c490773c9da922ccd0e25a97796e9
Greg Flynn <flynng@lafayette.edu>
2015-07-02 13:34:26 -0400

    Added in previous alive to columns, need to test, working on #30

--- srcs/array/life_col.v
+++ srcs/array/life_col.v
@@ -31,25 +31,26 @@ module life_col4(input clk,
 		 input [3:0] val,	 
 		 // This is the status of all of the cells
 		 output [3:0] alive_col,
+         output [3:0] alive_prev_col,
 		 
 		 // this is to run the entire column
 		 input enable
 		 );
     
    life_cell LC0(.clk(clk), .reset(reset), .n(n), .ne(ne), .e(e_col[0]),
-        .write(write_enb), .val(val[0]), .enb(enable),
+        .write(write_enb), .val(val[0]), .enb(enable), .alive_prev(alive_prev_col[0]),
 		 .se(e_col[1]), .s(alive_col[1]), .sw(w_col[1]), .w(w_col[0]), .nw(nw), .alive(alive_col[0]));
 
    life_cell LC1(.clk(clk), .reset(reset), .n(alive_col[0]), .ne(e_col[0]), .e(e_col[1]),
-         .write(write_enb), .val(val[1]), .enb(enable),
+         .write(write_enb), .val(val[1]), .enb(enable), .alive_prev(alive_prev_col[1]),
 		 .se(e_col[2]), .s(alive_col[2]), .sw(w_col[2]), .w(w_col[1]), .nw(w_col[0]), .alive(alive_col[1]));
 		 
    life_cell LC2(.clk(clk), .reset(reset), .n(alive_col[1]), .ne(e_col[1]), .e(e_col[2]),
-               .write(write_enb), .val(val[2]), .enb(enable),
+               .write(write_enb), .val(val[2]), .enb(enable), .alive_prev(alive_prev_col[2]),
                .se(e_col[3]), .s(alive_col[3]), .sw(w_col[3]), .w(w_col[2]), .nw(w_col[1]), .alive(alive_col[2]));
 
    life_cell LC3(.clk(clk), .reset(reset), .n(alive_col[2]), .ne(e_col[2]), .e(e_col[3]),
-               .write(write_enb), .val(val[3]), .enb(enable),
+               .write(write_enb), .val(val[3]), .enb(enable), .alive_prev(alive_prev_col[3]),
                .se(se), .s(s), .sw(sw), .w(w_col[3]), .nw(w_col[2]), .alive(alive_col[3]));
 		 
 endmodule // life_col4

ec05c5a2756912f38d6109b892907cc2e1854b87
gregf36665 <flynng@lafayette.edu>
2015-07-21 17:19:51 -0400

    Worked more on generated array

--- life_prototype/pe.sv
+++ life_prototype/pe.sv
@@ -21,15 +21,15 @@
 `include "pe_decs.sv"
 
 module pe (input clk, rst,
-	   input 		       rsel, csel, // used for read, write
-	   input [`PE_CMD_BITS-1:0]    cmd,
-	   output [`N_STATUS_BITS-1:0] status_out, // for communicating with neighbors
-	   input [`PE_STATE_BITS-1:0]  state_in,
-	   output reg [`PE_STATE_BITS-1:0] state_out,
-	   output 		       active, // for keeping track of activity in entire array - true when state changes
+       input 		       rsel, csel, // used for read, write
+       input [`PE_CMD_BITS-1:0]    cmd,
+       output [`N_STATUS_BITS-1:0] status_out, // for communicating with neighbors
+       input [`PE_STATE_BITS-1:0]  state_in,
+       output reg [`PE_STATE_BITS-1:0] state_out,
+       output 		       active, // for keeping track of activity in entire array - true when state changes
 
-	   input [`N_STATUS_BITS-1:0]  w_i, e_i, n_i, s_i, // manhattan neighbors
-   	   input [`N_STATUS_BITS-1:0]  nw_i, ne_i, sw_i, se_i);   // diagonal neighbors
+       input [`N_STATUS_BITS-1:0]  w_i, e_i, n_i, s_i, // manhattan neighbors
+       input [`N_STATUS_BITS-1:0]  nw_i, ne_i, sw_i, se_i);   // diagonal neighbors
 
    // PE selection logic
 
@@ -56,7 +56,7 @@ module pe (input clk, rst,
    logic [3:0] 			 neighbor_count;
 
    assign neighbor_count = w_i + n_i + e_i + s_i
-	  + nw_i + ne_i + sw_i + se_i;
+      + nw_i + ne_i + sw_i + se_i;
 
 
    always_ff @(posedge clk)
@@ -67,29 +67,24 @@ module pe (input clk, rst,
      begin
      state_out = state; // default beavhior
      nstate = state;
-	case (cmd)
-	  `PE_CMD_NOP:
-	    nstate = state;  // do nothing!
-	  `PE_CMD_PROCESS:
-	    if (neighbor_count < 2) nstate = `PE_STATE_DEAD;
-	    else if (neighbor_count == 2) nstate = state;
-	    else if (neighbor_count == 3) nstate = `PE_STATE_LIVE;
-	    else nstate = `PE_STATE_DEAD;
-	  `PE_CMD_READ:
-	    if (sel) state_out = state;
-	    else state_out = 0;
-	  `PE_CMD_WRITE:
-	    if (sel) nstate = state_in;
-	    else nstate = state;
-	  default: nstate = state;
-	 endcase // case(cmd)
+    case (cmd)
+      `PE_CMD_NOP:
+        nstate = state;  // do nothing!
+      `PE_CMD_PROCESS:
+        if (neighbor_count < 2) nstate = `PE_STATE_DEAD;
+        else if (neighbor_count == 2) nstate = state;
+        else if (neighbor_count == 3) nstate = `PE_STATE_LIVE;
+        else nstate = `PE_STATE_DEAD;
+      `PE_CMD_READ:
+        if (sel) state_out = state;
+        else state_out = 0;
+      `PE_CMD_WRITE:
+        if (sel) nstate = state_in;
+        else nstate = state;
+      default: nstate = state;
+     endcase // case(cmd)
      end // always_comb
 
-   
-   
-
-   
-   
 
    assign active = nstate != state;
 
@@ -101,4 +96,4 @@ endmodule // pe
 
 
 
-	   
+       
